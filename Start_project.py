e82063b93c84e1fa22d9b7043b4537ac6e2554cf
William_M <106087957+WilliamOrNotWilliam@users.noreply.github.com>
2023-01-26 15:32:11 +0100

    Add files via upload

---
+++ Start_project.py
@@ -0,0 +1,34 @@
+import signal
+import os
+import random
+import argparse
+
+
+port = random.randint(50000, 55000)
+
+
+def handle_sigint(signum, frame):
+    print("\n\nFERMETURE DE LA FERME\n\n")
+    os._exit(0)
+
+
+def main(f) -> int:
+
+    os.system(
+        "cls && echo LANCEMENT DES TESTS && pytest --cov= "
+        "&& echo LANCEMENT DE MYPY && mypy farm.py "
+        f"&& echo LANCEMENT DE LA FERME && start /B pythonw3 -m chronobio.game.server {f} -p {port} "
+        f"&& python3 sample_player_client.py -p {port}"
+    )
+    return 0
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--fast", help="Mode fast", action="store_true")
+    args = parser.parse_args()
+
+    fast = "--fast" if args.fast else ""
+
+    main(fast)
